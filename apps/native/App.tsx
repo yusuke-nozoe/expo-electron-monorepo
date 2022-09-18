187ad1f19468d1c95d359c844d811b1ebbe1a4a1
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-09-18 21:33:37 +0900

    rename directory

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Button from 'ui/atoms/Button'
+import Button from 'ui/components/atoms/Button'
 
 export default function App() {
   return (
