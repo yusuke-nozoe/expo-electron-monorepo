811eb2ed3e75976538a882780209ab4f5bacbaaf
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2022-09-18 23:17:42 +0900

    nicely handle typescript

--- apps/native/electron/webpack.config.js
+++ apps/native/electron/webpack.config.js
@@ -2,5 +2,7 @@ const { withExpoWebpack } = require('@expo/electron-adapter');
 
 module.exports = config => {
   config.resolve.alias['ui'] = '../../packages/ui';
+  config.resolve.extensions.push('.tsx')
+  config.externals.splice(config.externals.indexOf('ui'), 1)
   return withExpoWebpack(config);
 };
